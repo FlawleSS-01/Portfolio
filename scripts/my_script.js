43cb56d949a7fe8a0ea17614b9d598e228180c4b
Your Name <your_email@example.com>
2025-10-19 01:48:07 +0300

    Enhanced index.html with new navigation items, updated work experience descriptions, and improved contact form functionality. Removed outdated photo assets and refined CSS for better responsiveness and visual appeal.

--- scripts/my_script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	document.querySelector(".contact__form").addEventListener("submit", function(event) {
-			event.preventDefault();
-
-			const formData = new FormData(this);
-			document.querySelector("button[type=submit]").innerText = "Спасибо!";
-
-	});
-});
